237717a2e3a1f33cfb7869d0a54d4e778dfbd975
Christian Limberg <cnlimberg@gmail.com>
2025-09-16 22:27:46 +0200

    banner animation

--- js/title_animation.js
+++ js/title_animation.js
@@ -2,34 +2,32 @@ function randint(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function animate_change(elem, new_content, callback) {
-    
+async function animate_change(elem, new_content, wait_min, wait_max) {
     let current_content = elem.innerHTML;
-    console.log(current_content);
-    if(current_content == new_content) {
-        if (callback) callback();  // ✅ done, trigger next
-        return;
-    }
+    while(true) {
+        if(current_content == new_content) {
+            return;
+        }
 
-    for(let i=0;i<new_content.length;++i) {
-        if(current_content.length > i) {
-            if(current_content[i] != new_content[i]) {
-                current_content[i] = new_content[i];
+        for(let i=0;i<new_content.length;++i) {
+            if(current_content.length > i) {
+                if(current_content[i] != new_content[i]) {
+                    current_content[i] = new_content[i];
+                    break;
+                }
+            } else {
+                current_content = current_content + new_content[i];
                 break;
             }
-        } else {
-            current_content = current_content + new_content[i];
-            break;
         }
-    }
-    if(current_content.length > new_content.length) {
-        current_content = current_content.substr(0,current_content.length-1); 
-    }
+        if(current_content.length > new_content.length) {
+            current_content = current_content.substr(0,current_content.length-1); 
+        }
 
-    elem.innerHTML = current_content;
-    let rand_timeout = randint(100,200);
-
-    return setTimeout(animate_change, rand_timeout, elem, new_content, callback);
+        elem.innerHTML = current_content;
+        let rand_timeout = randint(wait_min,wait_max);
+        await sleep(rand_timeout);
+    }
 }
 
 let title1 = document.getElementById('header_title_1');
@@ -38,78 +36,45 @@ let bannerImg = document.querySelector('.header-logo');
 
 // Array of banner images to cycle through
 const bannerImages = [
-    'banner_1.png', 
-    'banner_2.png',
-    'banner_3.png',
-    'banner_4.png'
+    'small_banner_1.png', 
+    'small_banner_2.png',
+    'small_banner_3.png',
+    'small_banner_4.png'
 ];
-
-let currentBannerIndex = 0;
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 // Function to change banner image with fade effect
-function changeBannerImage(newImageSrc, callback) {
+function changeBannerImage(newImageSrc) {
     if (!bannerImg) return;
-    
-    // Create a new image element to preload the image
     const newImg = new Image();
-    
     newImg.onload = () => {
-
-        setTimeout(() => {
-            // Change the source
-            bannerImg.src = newImageSrc;
-            bannerImg.alt = 'VisArt Logo';
-            
-            // Call callback after fade in completes
-            setTimeout(() => {
-                if (callback) callback();
-            }, 500);
-        }, 500);
+        bannerImg.src = newImageSrc;
+        bannerImg.alt = 'VisArt Logo';
     };
-    
-    newImg.onerror = () => {
-        console.warn(`Failed to load banner image: ${newImageSrc}`);
-        if (callback) callback();
-    };
-    
-    // Start loading the new image
     newImg.src = newImageSrc;
 }
 
-// Function to cycle through banner images
-function cycleBannerImages() {
-    currentBannerIndex = (currentBannerIndex + 1) % bannerImages.length;
-    const nextBanner = bannerImages[currentBannerIndex];
-    
-    changeBannerImage(nextBanner, () => {
-        // Schedule next banner change
-        setTimeout(cycleBannerImages, randint(100, 200)); // 5-15 minutes
-    });
-}
 
 async function title_animate() {
-    await banner_animate([0,1,2,3]);
-    await animate_change(title1, 'visualize', () => {
-        animate_change(title2, 'articulate', () => {
-            animate_change(title2, 'art', () => {
-                    animate_change(title1, 'vis');
-            })
+    await banner_animate([0,1,2,3], [50,50,50,50]);
 
-        });
-    });
-    banner_animate([3,2,1,0]);
+    await animate_change(title1, 'visualize', 20, 50);
+    await animate_change(title2, 'articulate', 20, 50);
+    await animate_change(title2, 'art', 20, 50);
+    await animate_change(title1, 'vis', 20, 50);
+    
+    await banner_animate([3,2,1,0], [200,50,50,50]);
     setTimeout(title_animate,randint(2*60*1000,30*60*1000));
 
 }
 
-async function banner_animate(frames) {
+async function banner_animate(frames, sleep_times) {
     for(let i=0;i<frames.length;i++) {
         changeBannerImage(bannerImages[frames[i]]);
-        await sleep(50);
+        await sleep(sleep_times[i]);
     }
 }
 
